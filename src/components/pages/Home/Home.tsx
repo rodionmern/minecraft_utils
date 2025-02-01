666057db78f4cc92085826556e1633aec977b3f5
rodionmern <rodionsaburov@disroot.org>
2025-02-01 17:44:27 +0300

    Added pictures (probably)

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -7,17 +7,20 @@ import Kopeechka from "../../ui/Kopeechka/Kopeechka"
 import Frame from "../../ui/Frame/Frame"
 import Button from "../../ui/Button/Button"
 
+import obsidian from  "../../../assets/obsidian.png"
+import end_portal_frame from"../../../assets/end_portal_frame.png" 
+
 const Home = () => {
   return (
     <Kopeechka>
         <h1>Minecraft Utils</h1>
         <div className="nepridymal">
             <div className="neprid">
-                <Frame icon="src\assets\img\obsidian.png"></Frame>
+                <Frame icon={obsidian}></Frame>
                 <Button><Link to="/nether" className="link">Nether</Link></Button>
             </div>
             <div className="neprid">
-                <Frame icon="src\assets\img\end_portal_frame.png"></Frame>
+                <Frame icon={end_portal_frame}></Frame>
                 <Button><Link to="/end" className="link">End</Link></Button>
             </div>
         </div>
